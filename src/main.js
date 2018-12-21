6a92b636dfce1f34dca45b155b648d0d74e3af24
Pierre Romera <hello@pirhoo.com>
2018-12-21 18:03:46 +0100

    First commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Murmur from '@icij/murmur'
+
+import './main.scss';
 
 Vue.config.productionTip = false
+
+Vue.use(Murmur)
 
 new Vue({
   render: h => h(App),
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { join } = require('path')
+const resolve = filepath => join(__dirname, filepath)
+
+module.exports = {
+  chainWebpack: config => {
+    // Aliases configuration
+    config.resolve.alias
+      .set('images', resolve('src/assets/images'))
+      .set('node_modules', resolve('node_modules'))
+  }
+}
