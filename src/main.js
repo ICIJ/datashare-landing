1cb426964fe0d83be4337a07ea37303aa8b919e7
Pierre Romera <hello@pirhoo.com>
2019-01-30 16:35:40 +0100

    Added tracker for donate form

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ Vue.config.productionTip = false
 
 Vue.use(Murmur)
 
+Murmur.config.set('signup-form.tracker', 'DATASHARE-LANDING')
+Murmur.config.set('donate-form.tracker', '701f4000000mfuf')
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
