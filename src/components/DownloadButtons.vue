be53d9d565a33fcc68256f9a789354c3ad9fb471
Pierre Romera <hello@pirhoo.com>
2020-05-06 11:58:56 +0200

    Added guide after downloading datashare

--- src/components/DownloadButtons.vue
+++ src/components/DownloadButtons.vue
@@ -1,34 +1,30 @@
 <template>
   <div class="download-buttons text-center">
-    <div class="row mb-2">
-      <div class="col mb-2">
-        <div v-if="os === 'macos'">
-          <a class="btn btn-block btn-lg btn-primary text-light download-buttons__button" :href="assetForApple">
-            <fa :icon="['fab', 'apple']" class="mr-2"></fa>
-            <span class="sr-only">Apple</span>
-            Download for free
+    <div class="row mb-2 no-gutters">
+      <div class="col mb-2 position-relative">
+        <div v-if="asset">
+          <a class="btn btn-block btn-lg btn-primary text-light download-buttons__button" :href="asset" @click="showGuide = !showGuide">
+            <fa :icon="['fab', icon]" class="mr-2"></fa>
+            <span class="sr-only">{{ os }}</span>
+            Download for free {{ assetIsStandalone }}
           </a>
-        </div>
-        <div v-else-if="os === 'windows'">
-          <a class="btn btn-block btn-lg btn-primary text-light download-buttons__button" :href="assetForWindows">
-            <fa :icon="['fab', 'windows']" class="mr-2"></fa>
-            <span class="sr-only">Windows</span>
-            Download for free
-          </a>
-        </div>
-        <div v-else-if="os === 'ubuntu'">
-          <a class="btn btn-block btn-lg btn-primary text-light download-buttons__button" :href="assetForUbuntu">
-            <fa :icon="['fab', 'ubuntu']" class="mr-2"></fa>
-            <span class="sr-only">Ubuntu</span>
-            Download for free
-          </a>
-        </div>
-        <div v-else-if="os === 'linux'">
-          <a class="btn btn-block btn-lg btn-primary text-light download-buttons__button" :href="assetForLinux">
-            <fa :icon="['fab', 'linux']" class="mr-2"></fa>
-            <span class="sr-only">Linux</span>
-            Download for free
-          </a>
+          <transition name="circle-in">
+            <div class="download-buttons__guide" v-if="showGuide">
+              <button @click="showGuide = false" class="download-buttons__guide__close btn btn-sm text-light rounded-circle">
+                <fa icon="times" />
+                <span class="sr-only">Close</span>
+              </button>
+              <p class="font-weight-bold">
+                Thanks for downloading Datashare
+              </p>
+              <p>
+                <a :href="guide || 'https://icij.gitbook.io/datashare/'" target="_blank">
+                  <fa icon="book" class="mr-2" />
+                  <u>Read the installation guide</u>
+                </a>
+              </p>
+            </div>
+          </transition>
         </div>
         <div v-else>
           <span class="btn btn-block btn-lg btn-outline-dark text-muted download-buttons__button download-buttons__button--disabled">
@@ -57,6 +53,7 @@
 <script>
   import { BModal } from 'bootstrap-vue'
   import { find, endsWith, get } from 'lodash'
+  import { faBook } from '@fortawesome/free-solid-svg-icons/faBook'
   import { faWindows } from '@fortawesome/free-brands-svg-icons/faWindows'
   import { faApple } from '@fortawesome/free-brands-svg-icons/faApple'
   import { faLinux } from '@fortawesome/free-brands-svg-icons/faLinux'
@@ -69,7 +66,7 @@
   import DownloadRequirements from './DownloadRequirements.vue'
   import DownloadVariants from './DownloadVariants.vue'
 
-  library.add(faApple, faWindows, faLinux, faUbuntu)
+  library.add(faApple, faBook, faWindows, faLinux, faUbuntu)
 
   export default {
     name: 'DownloadButtons',
@@ -85,6 +82,7 @@
         version: null,
         year: null,
         assets: [],
+        showGuide: false
       }
     },
     async mounted () {
@@ -94,17 +92,48 @@
       this.year = new Date(data.created_at).getFullYear()
     },
     computed: {
-      assetForApple () {
-        return get(find(this.assets, a => endsWith(a.name, '.pkg') ), 'browser_download_url', null)
+      osAssets () {
+        return {
+          macos: {
+            url: this.assetForMacOs,
+            icon: 'apple',
+            guide: 'https://icij.gitbook.io/datashare/mac/install-datashare-on-mac#standalone-less-recent-than-os-x-el-capitan-10-11'
+          },
+          windows: {
+            url: this.assetForWindows,
+            icon: 'windows',
+            guide: 'https://icij.gitbook.io/datashare/windows/install-datashare-on-windows#standalone-windows-7-service-pack-2-or-newer-version'
+          },
+          ubuntu: {
+            url: this.assetForUbuntu,
+            icon: 'ubuntu'
+          },
+          linux: {
+            url: this.assetForLinux,
+            icon: 'linux'
+          }
+        }
+      },
+      assetForMacOs () {
+        return get(find(this.assets, a => endsWith(a.name, 'Standalone.pkg') ), 'browser_download_url', null)
       },
       assetForWindows () {
-        return get(find(this.assets, a => endsWith(a.name, '.exe') ), 'browser_download_url', null)
+        return get(find(this.assets, a => endsWith(a.name, 'Standalone.exe') ), 'browser_download_url', null)
       },
       assetForLinux () {
         return get(find(this.assets, a => endsWith(a.name, '.sh') ), 'browser_download_url', null)
       },
       assetForUbuntu () {
         return get(find(this.assets, a => endsWith(a.name, '.deb') ), 'browser_download_url', null)
+      },
+      asset () {
+        return get(this.osAssets, [this.os, 'url'], null)
+      },
+      icon () {
+        return get(this.osAssets, [this.os, 'icon'], null)
+      },
+      guide () {
+        return get(this.osAssets, [this.os, 'guide'], null)
       }
     }
   }
@@ -131,6 +160,48 @@
     &__button--disabled.btn {
       text-transform: none;
       background: white;
+    }
+
+    &__guide {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      z-index: 100;
+      padding: $spacer * 1.4 $spacer;
+      border-radius: $border-radius-lg;
+      color: white;
+      @include gradient-directional($secondary, mix($primary, $secondary));
+
+      a {
+        color: white;
+      }
+
+      &__close {
+        position: absolute;
+        top: $spacer * .5;
+        right: $spacer * .5;
+        width: 1.7em;
+        height: 1.7em;
+        line-height: 1.7em;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+      }
+
+      &.circle-in-enter-active,
+      &.circle-in-leave-active {
+        transition: all .3s ease;
+        clip-path: circle(100% at center);
+      }
+
+      &.circle-in-enter,
+      &.circle-in-leave-to {
+        clip-path: circle(0% at center);
+      }
     }
 
     &__help {
