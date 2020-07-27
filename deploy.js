f5661e13503a8c9d1b5d7ea4570bfac09a8b9e38
Pierre Romera <hello@pirhoo.com>
2020-07-27 11:23:05 +0200

    Removed ora

--- deploy.js
+++ deploy.js
@@ -1,14 +1,9 @@
 const gh = require('gh-pages');
 const path = require('path');
-const ora = require('ora')
 const chalk = require('chalk')
 
 process.env.NODE_ENV = 'production'
 
-const spinner = ora('push dist folder to gh-pages branch...')
-spinner.start()
-
 gh.publish(path.join(process.cwd(), 'dist'), () => {
-  spinner.stop()
   console.log(chalk.cyan('  Push complete.\n'))
 });
