18cac66b7fc4daebc8b5ff5c6ada53194938e59b
Pierre Romera <hello@pirhoo.com>
2022-06-20 10:57:36 +0200

    test: add UA for Chrome on Ubuntu 22.04

--- tests/unit/os.spec.js
+++ tests/unit/os.spec.js
@@ -10,5 +10,6 @@ describe('os detection tests', () => {
 
   it('detects linux distribution', () => {
     expect(family_internal('Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0')).toMatch('linux')
+    expect(family_internal('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36')).toMatch('linux')
   })
 })
