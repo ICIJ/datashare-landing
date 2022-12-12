fcbab50171c5947cfe8213cfb108172b53dfcb8f
Pierre Romera <hello@pirhoo.com>
2022-12-12 12:01:39 +0100

    feat: update required Mac version

--- src/components/DownloadRequirements.vue
+++ src/components/DownloadRequirements.vue
@@ -3,7 +3,7 @@
     <h6>Requirements</h6>
     <ul class="pl-4">
       <li>Windows: Windows 7 Service Pack 2 or higher</li>
-      <li>Mac: High Sierra or higher</li>
+      <li>Mac: Catalina or higher</li>
       <li>Linux: most modern versions (Java JRE8)</li>
       <li>5 GB of free space on disk</li>
       <li>4 GB of RAM</li>
