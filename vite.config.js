dbf237a2b4b651d85b30a4fb4f7389c0d4fd8c5b
Caroline Desprat <cdesprat@icij.org>
2023-02-13 11:34:36 +0000

    build: move from webpack to vite
    Co-authored-by: Pierre Romera <pirhoo@users.noreply.github.com>

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vuePlugin from '@vitejs/plugin-vue2'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  test: {
+    globals: true,
+    reporter: "basic",
+    environment: "jsdom",
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 9009,
+  },
+  plugins: [
+    vuePlugin({
+      include: [/\.vue$/, /\.md$/],
+    }),
+  ],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    alias: {
+      node_modules: resolve(__dirname, "node_modules"),
+      images: resolve(__dirname, "src/assets/images"),
+    },
+  },
+})
