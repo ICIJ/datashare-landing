1698d207b4a8b0bb13d7290eddac07aa256df74a
Caroline Desprat <cdesprat@icij.org>
2025-06-25 09:52:56 +0000

    style: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,31 +1,35 @@
-import { globalIgnores,  } from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
-import compat from "./bin/eslint/compat"
-import { globals as iconsGlobals } from "./bin/icons"
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import compat from './bin/eslint/compat'
+import { globals as iconsGlobals } from './bin/icons'
 
 export default defineConfigWithVueTs(
   {
     files: ['**/*.{ts,mts,tsx,vue}'],
+    languageOptions: {
+      globals: {
+        ...Object.fromEntries(Object.keys(iconsGlobals).map((key) => [key, 'readonly']))
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^(Ph[A-Z])|(_)' }],
+      'vue/no-v-model-argument': 'off',
+      'vue/no-v-for-template-key': 'off',
+      'vue/no-custom-modifiers-on-v-model': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/valid-v-slot': 'off',
+      'import/extensions': 'off'
+    }
   },
-  globalIgnores([
-    '**/dist/**',
-    '**/dist-ssr/**',
-    '**/coverage/**',
-    'public/**',
-    'src/assets/**',
-    'node_modules']),
-  ...compat.extends("@icij/eslint-config-icij"),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'public/**', 'src/assets/**', 'node_modules']),
+  ...compat.extends('@icij/eslint-config-icij'),
   pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
     ...pluginVitest.configs.recommended,
-    files: ['tests/*'],
+    files: ['tests/*']
   }
 )
