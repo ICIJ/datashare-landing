836d370d2060b72f6c9c476d24ebd980770c604b
Caroline Desprat <cdesprat@icij.org>
2025-06-25 16:20:39 +0000

    Revert: fix: add ora package to deploy

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 import gh from 'gh-pages'
 import path from 'path'
 import chalk from 'chalk'
-import ora from 'ora'
 
 process.env.NODE_ENV = 'production'
-const spinner = ora('push dist folder to gh-pages branch...')
-spinner.start()
 
-gh.publish(path.join(process.cwd(), 'dist'), () => {
-  spinner.stop()
-  console.log(chalk.cyan('  Push complete.\n'))
+gh.publish(path.join(process.cwd(), 'dist'), (err) => {
+  if(err){
+    console.error("error ",err)
+  } else {
+    console.log(chalk.cyan('  Push complete.\n'))
+  }
 })
