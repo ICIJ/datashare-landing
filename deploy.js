f6b08499c79676649c7f4fc83d0c4f067e1644c5
Caroline Desprat <cdesprat@icij.org>
2025-06-25 15:33:15 +0000

    deploy: add spinner back

--- deploy.js
+++ deploy.js
@@ -1,9 +1,13 @@
 import gh from 'gh-pages'
 import path from 'path'
 import chalk from 'chalk'
+const ora = require('ora')
 
 process.env.NODE_ENV = 'production'
+const spinner = ora('push dist folder to gh-pages branch...')
+spinner.start()
 
 gh.publish(path.join(process.cwd(), 'dist'), () => {
+  spinner.stop()
   console.log(chalk.cyan('  Push complete.\n'))
 })
