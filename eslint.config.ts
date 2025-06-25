ba80abd69779601427332ab27812703545978755
Caroline Desprat <cdesprat@icij.org>
2025-06-25 09:48:43 +0000

    style: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,9 @@
-import { globalIgnores } from 'eslint/config'
+import { globalIgnores,  } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
+import compat from "./bin/eslint/compat"
+import { globals as iconsGlobals } from "./bin/icons"
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -10,17 +12,20 @@ import pluginVitest from '@vitest/eslint-plugin'
 
 export default defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
+  globalIgnores([
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**',
+    'public/**',
+    'src/assets/**',
+    'node_modules']),
+  ...compat.extends("@icij/eslint-config-icij"),
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
-
   {
     ...pluginVitest.configs.recommended,
     files: ['tests/*'],
-  },
+  }
 )
