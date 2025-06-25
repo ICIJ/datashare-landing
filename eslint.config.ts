f25c6bef5dc68b976e24af391edae793b7c05677
Caroline Desprat <cdesprat@icij.org>
2025-06-25 15:31:48 +0000

    chore: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,19 +2,12 @@ import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
+import pluginImport from 'eslint-plugin-import'
+import stylistic from '@stylistic/eslint-plugin'
 
-import compat from './bin/eslint/compat'
 import { globals as iconsGlobals } from './bin/icons'
 
 export default defineConfigWithVueTs(
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'public/**', 'src/assets/**', 'node_modules']),
-  pluginVue.configs['flat/recommended'],
-  vueTsConfigs.recommended,
-  ...compat.extends('@icij/eslint-config-icij'),
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['tests/*']
-  },
   {
     files: ['**/*.{ts,mts,tsx,vue}'],
     languageOptions: {
@@ -22,14 +15,57 @@ export default defineConfigWithVueTs(
         ...Object.fromEntries(Object.keys(iconsGlobals).map((key) => [key, 'readonly']))
       }
     },
+    plugins: {
+      '@stylistic': stylistic,
+      import: pluginImport
+    },
     rules: {
       'no-unused-vars': ['error', { argsIgnorePattern: '^(Ph[A-Z])|(_)' }],
-      'vue/no-v-model-argument': 'off',
-      'vue/no-v-for-template-key': 'off',
-      'vue/no-custom-modifiers-on-v-model': 'off',
-      'vue/no-multiple-template-root': 'off',
-      'vue/valid-v-slot': 'off',
-      'import/extensions': 'off'
+
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always'
+        }
+      ],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'only-multiline'],
+      '@stylistic/curly-newline': ['error', 'always'],
+      'vue/max-attributes-per-line': ['error', {
+        'singleline': {
+          'max': 3
+        },
+        'multiline': {
+          'max': 1
+        }
+      }]
+    }
+  },
+  globalIgnores([
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**',
+    'public/**',
+    'src/assets/**',
+    'node_modules'
+  ]),
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['tests/*']
+  },
+  {
+    rules:{
+      'vue/no-v-html': 'off',
+      'vue/require-default-prop':'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     }
   }
 )
