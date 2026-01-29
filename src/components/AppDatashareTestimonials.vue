c8ef512b0311af8032148438598b3d56cfc13d25
Pierre Romera <hello@pirhoo.com>
2026-01-29 12:17:00 +0000

    refactor: use id as v-for key in testimonials

--- src/components/AppDatashareTestimonials.vue
+++ src/components/AppDatashareTestimonials.vue
@@ -5,6 +5,7 @@ import DatashareTestimonial from '@/components/DatashareTestimonial/DatashareTes
 import AppSection from '@/components/AppSection.vue'
 
 interface Testimonial {
+  id: string
   quote: string
   author: string
   logo?: string
@@ -13,36 +14,42 @@ interface Testimonial {
 
 const testimonials: Testimonial[] = [
   {
+    id: 'lemonde',
     quote: 'A powerful easy-to-use tool indispensable to my investigations',
     author: 'Abdelhak El Idrissi, France',
     logo: 'LeMonde',
     affiliation: 'Le Monde'
   },
   {
+    id: 'occrp',
     quote: 'I batch-search hundreds of people across millions of documents and get story leads in one go',
     author: 'Purity Mukami, Kenya',
     logo: 'OCCRP',
     affiliation: 'OCCRP'
   },
   {
+    id: 'indian-express',
     quote: 'A treasure trove! I land a single clinching document or a file with hundreds of pages for unravelling a subject',
     author: 'Ritu Sarin, India',
     logo: 'TheIndianExpress',
     affiliation: 'The Indian Express'
   },
   {
-    quote: 'Open Source “Best Project”',
+    id: 'acteurs-du-libre',
+    quote: 'Open Source "Best Project"',
     author: 'Paris Open Source Summit',
     logo: 'LesActeursDuLibre',
     affiliation: 'Les Acteurs Du Libre'
   },
   {
+    id: 'kcij',
     quote: 'Essential tool for small newsrooms. Batch-search and auto-translation are a big help',
     author: 'Jiyoon Kim, South Korea',
     logo: 'KCIJ',
     affiliation: 'KCIJ'
   },
   {
+    id: 'katharina-brunner',
     quote: 'A must-have for efficiently scanning, filtering, and searching through a large collection of documents.',
     author: 'Katharina Brunner, Germany'
   }
@@ -60,8 +67,8 @@ const testimonials: Testimonial[] = [
     <div class="remove-outer-line">
       <div class="remove-double-line row row-wrap justify-content-center ">
         <datashare-testimonial
-          v-for="(testimonial,i) in testimonials"
-          :key="i"
+          v-for="testimonial in testimonials"
+          :key="testimonial.id"
           v-bind="testimonial"
         />
       </div>
