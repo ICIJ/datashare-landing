9ff8e31f18617d7fdd894d9352c2e9069e2818a3
Pierre Romera <hello@pirhoo.com>
2026-01-29 11:36:00 +0000

    refactor: remove eslint compat

--- bin/eslint/compat.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const filepath = fileURLToPath(import.meta.url)
-const baseDirectory = path.dirname(filepath)
-const compat = new FlatCompat({ baseDirectory })
-
-export default compat
